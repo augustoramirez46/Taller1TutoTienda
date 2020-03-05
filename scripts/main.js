cc9dfbc3788d268538403d8d6971d7bba6d4d9a1
augustoramirez46 <tutoead46@gmail.com>
2020-03-05 17:04:22 -0500

    todos los widths son 100%. Inicio de responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+var hambtn = document.querySelector('.hamburguerbtn');
+var mainnav = document.querySelector('.nav');
+
+var handleHamClick = function (event) {
+    console.log(event);
+    mainnav.classList.toggle('nav--mobile');
+}
+
+hambtn.addEventListener('click', handleHamClick);
+
+
+
+
+
+
+
+
+
+
+
 var buttonCalc = document.querySelector('.publicidadCalcas__button');
 
 function botonCalcClick() {
